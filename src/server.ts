75ffdedc3e06b04b35688a1df954a626a36fa6da
JunioResende <junioresende23@gmail.com>
2021-02-28 09:54:21 -0300

    start

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+app.listen(3333, () => {
+  console.log('🚀NLW server its running on port 3333!...');
+});
